4435c2eed00188268f60e4c6a09b1e48ad02e093
MelBurdisso1 <missmelburdisso@gmail.com>
2022-08-23 14:39:18 -0300

    realized

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,15 +6,15 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
- let result=[1]
- let i=2;
- while (num>1){
+ 
+ let result=[1]//si o si va a estar porque num/1 va a dar el mismo num
+ let i=2; //arranco 2 porque necesito el resultado del indice anterior para poder seguir dividiendo, si arranco en 0 o 1 es irrelevante
+ while (num>1){ //mientras el parámetro num sea mayor a 1 voy a dividir, y si su resto da 0 lo voy a agregar al array result
    if(num%i===0){
      result.push(i);
-     num = num/i;
+     num = num/i; //luego de agregar el num al array, declaro que num va a ser igual al resultado de num dividido anteriormente
     } else {
-      i++;
+      i++; //en el caso que el resto no sea 0, avanzo un numero más en i para seguir dividiendo
     }
   }
    return result;
@@ -30,14 +30,14 @@ function bubbleSort(array) {
   /* recorre buscando el menor del q le sigue;
   recorre para ubicar el num donde corresponde
   */
- let swap=true;
- while(swap){
+ let swap=true;// si se hace swap se recorre, sino no porque esta ordenado
+ while(swap){//si en algun momento hay un swap se pasa a true
     swap=false;
-    for (let i= 0; i < array.length; i++) {
-      if (array[i]>array[i+1]){
-        let aux=array[i];
-        array[i]=array[i+1];
-        array[i+1]=aux;
+    for (let i= 0; i < array.length; i++) {//recorremos el array para verificar si hay que ordenar
+      if (array[i]>array[i+1]){//comparo el valor actual con el siguiente
+        let aux=array[i];//guardo el valor en una variable auxiliar para luego swapear
+        array[i]=array[i+1];//corro de indice el aux que guardé, pero al quedar copiados(iguales)
+        array[i+1]=aux;//muevo de indice o posicion asi se realiza el swap.
         swap=true;
       }     
     }
@@ -50,14 +50,14 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
- for (let i = 1; i < array.length; i++) {
-    let j=i-1;
-    let aux=array[i];
-    while (j>=0 && array[j]>aux) {
-      array[j+1]=array[j];
+ for (let i = 1; i < array.length; i++) {//recorro el array, arranco en 1 porque necesito comparar con un indice anterior
+    let j=i-1;//declaro variable j que me indica el anterior de i
+    let aux=array[i];// declaro una variable auxiliar para guardar en caso de swap
+    while (j>=0 && array[j]>aux) {//mientras el indice de j sea mayor o igual a 0 y array subindice de j se mayor que el valor que me guardo en la variable auxiliar(comparación de indices)
+      array[j+1]=array[j];//voy a swapear, al quedar copiados iguales, lo acomodo en una posicion anterior
       j--;
     }
-    array[j+1]=aux;
+    array[j+1]=aux; //coloco en el lugar correcto el indice
   } 
   return array;
 }
@@ -68,17 +68,17 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
- for (let i = 0; i < array.length; i++) {
-   let min = i;
+ for (let i = 0; i < array.length; i++) {//recorro el array buscando el menor valor
+   let min = i;//declaro variable auxiliar, valor minimo de i
    for (let j = i+1 ; j < array.length; j++) {
-     if (array[j]<array[min]) {
-       min =j;
+     if (array[j]<array[min]) {//si se cumple la condicion de que el valor del indice j sea menor que el valor minimo de i
+       min =j;//voy a declarar un nuevo min
       }
     }
-   if(i!== min){
-     let aux=array[i];
-     array[i]=array[min];
-     array[min]= aux;
+   if(i!== min){//si i es diferente del minimo
+     let aux=array[i];//se hace el intercambio
+     array[i]=array[min];//se igualan los indices, al quedar copiados, se intercambias posiciones
+     array[min]= aux;//se termina de acomodar el nuevo valor.
     }
   }return array;
 }
--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,27 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  /* 1 dividir el array al medio
+  2 determinar pivote
+  3 ordenar menores a la izq y mayores a la  derecha
+  4 recursion
+  5 unir y retornar*/ 
 
+  if(array.length<=1) return array;// en primer lugar me aseguro que si el array es menor o igual a 1, voy a retornar el array como está.
+  
+ 
+  let left=[];//declaro tres variables que definen la division del array en dos partes , minimos a la izq, mayores a la der y el pivot que va a ser el primer elemento del array
+  let right=[];
+  let pivot= array.shift();
+
+  for (let i of array) {// para cada item del array se va a ejecutar el mismo bloque, no nos interesa el indice sino el valor de cada indice.
+    if(pivot>i){//si el valor de i es menor al pivot, éstese va a agregar a la izquierda del pivot
+      left.push(i)
+    } else {//en caso contrario, se agregara a la derecha
+      right.push(i);
+    }
+  }
+  return [...quickSort(left), pivot, ...quickSort(right)];// en este paso se unen las funciones + el valor del pivote y se retorna el array ordenado. Los tres puntitos sirven para concatenar los diferentes bloques y unir todo en un solo array, en este caso
 }
 
 function mergeSort(array) {
@@ -14,8 +34,30 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if(array.length<=1) return array;//me aseguro que el array tenga más de un elemento, sino no se puede dividir.
 
+  let i= Math.floor(array.length/2);//declaro la variable i que va a ser el punto de division de mi array. uso math floor para que sea lo más exacta posible la particion
+  let left=array.slice(0,i);//al quedar dividido en dos el array, declaro que la parte derecha va a ser la division que inicia desde el indice o hasta el punto de division(i)
+  let right=array.slice(i);//la otra parte de la division va a comprender desde el punto de division(i) hasta el ultimo elemento que no se sabe.
+
+  left= mergeSort(left);//se llama con recursion a la funcion ya que en cada "lado" de la division se va a realizar el mismo bloque de procedimientos.
+  right= mergeSort(right);
+
+  let result= [];//declaro una variable result que va a ser el array ordenado
+
+  while(left.length && right.length){// mientras cada variable tenga elementos, voy a recorrer
+    if(left[0]>right[0]) {//comparo si el valor de izq es mayor al de la derecha
+      result.push(right.shift());// en caso de ser true, saco el valor del indice de right y lo pusheo a la variable de resultado
+    } else {
+      result.push(left.shift());// en el caso contrario, quito y pusheo el valor del indice de la izq
+    }
+  }
+ return [...result,...left,...right]//concateno y retorno el array ordenado
 }
+
+
+
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
